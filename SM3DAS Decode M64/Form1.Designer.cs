60d7db4ddb7707a8ce0078a66e01c6edc642ed9a
DanielSvoboda <daniel.svoboda@hotmail.com>
2025-03-24 21:08:07 -0300

    Initial commit

---
+++ SM3DAS Decode M64/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SM3DAS_Decode_M64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Decode = new System.Windows.Forms.Button();
+            this.button_Encode = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Decode
+            // 
+            this.button_Decode.Location = new System.Drawing.Point(12, 12);
+            this.button_Decode.Name = "button_Decode";
+            this.button_Decode.Size = new System.Drawing.Size(75, 23);
+            this.button_Decode.TabIndex = 0;
+            this.button_Decode.Text = "Decode";
+            this.button_Decode.UseVisualStyleBackColor = true;
+            this.button_Decode.Click += new System.EventHandler(this.button_Decode_Click);
+            // 
+            // button_Encode
+            // 
+            this.button_Encode.Enabled = false;
+            this.button_Encode.Location = new System.Drawing.Point(93, 12);
+            this.button_Encode.Name = "button_Encode";
+            this.button_Encode.Size = new System.Drawing.Size(75, 23);
+            this.button_Encode.TabIndex = 1;
+            this.button_Encode.Text = "Encode";
+            this.button_Encode.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1641, 702);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1666, 756);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Encode);
+            this.Controls.Add(this.button_Decode);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SM3DAS Decode M64";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Decode;
+        private System.Windows.Forms.Button button_Encode;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SM3DAS Decode M64/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SM3DAS_Decode_M64
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Decode_Click(object sender, EventArgs e)
+        {
+            // Permitir que o usuário escolha o arquivo Messages.bin
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Messages.bin (*.bin)|*.bin|All files (*.*)|*.*";
+                openFileDialog.Title = "Select file Messages.bin";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    DecodeFile(filePath);
+                }
+            }
+        }
+
+        private void DecodeFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("File not found.");
+                return;
+            }
+
+            try
+            {
+                byte[] fileData = File.ReadAllBytes(filePath);
+
+                int startAddress = 0x000009B0;
+
+                if (fileData.Length <= startAddress)
+                {
+                    MessageBox.Show("The file is smaller than the starting address.");
+                    return;
+                }
+
+                string decodedText = "";
+
+                for (int i = startAddress; i < fileData.Length; i++)
+                {
+                    byte modifiedByte = (byte)(fileData[i] + 61);
+
+
+                    // Simbolos:    ! " # $ % & ' ( ) * + , - . /
+                    if (modifiedByte >= 32 && modifiedByte <= 47)
+                    {
+                        modifiedByte = (byte)(modifiedByte - 14);
+                    }
+
+                    // Letras maiúsculas:   @ (A-Z) [ \ ] ^ _ `
+                    if (modifiedByte >= 64 && modifiedByte <= 96)
+                    {
+                        modifiedByte = (byte)(modifiedByte - 6);
+                    }
+
+
+
+                    if (modifiedByte == 219)
+                    {
+                        decodedText += ' ';
+                    }
+
+                    // Numeros de 0 a 9
+                    //if (modifiedByte >= 48 && modifiedByte <= 57)
+                    //{
+                    //    modifiedByte = (byte)(modifiedByte - 7);
+                    //}
+                    //else if (modifiedByte == 55)
+                    //{
+                    //    decodedText += '0';
+                    //}
+                    //else if (modifiedByte == 56)
+                    //{
+                    //    decodedText += '1';
+                    //}
+                    //else if (modifiedByte == 57)
+                    //{
+                    //    decodedText += '2';
+                    //}
+                    //else if (modifiedByte == 58)
+                    //{
+                    //    decodedText += '3';
+                    //}
+                    //else if (modifiedByte == 59)
+                    //{
+                    //    decodedText += '4';
+                    //}
+                    //else if (modifiedByte == 60)
+                    //{
+                    //    decodedText += '5';
+                    //}
+                    //else if (modifiedByte == 61)
+                    //{
+                    //    decodedText += '6';
+                    //}
+                    //else if (modifiedByte == 62)
+                    //{
+                    //    decodedText += '7';
+                    //}
+
+                    else if (modifiedByte == 62)
+                    {
+                        decodedText += "15";
+                    }
+                    else if (modifiedByte == 63)
+                    {
+                        decodedText += '8';
+                    }
+
+                    else if (modifiedByte == 220)
+                    {
+                        decodedText += '-';
+                    }
+                    else if (modifiedByte == 172)
+                    {
+                        decodedText += ',';
+                    }
+
+                    else if (modifiedByte == 124)
+                    {
+                        decodedText += '.';
+                    }
+
+                    else if (modifiedByte == 123)
+                    {
+                        decodedText += '\'';
+                    }
+
+
+
+                    //if (modifiedByte >= 32 && modifiedByte <= 63)
+                    //{
+                    //    modifiedByte = (byte)(modifiedByte - 14);
+                    //}
+
+
+                    else if (modifiedByte == 59)
+                    {
+                        decodedText += "\\n";
+                    }
+                    else if (modifiedByte == 60) // End of text block
+                    {
+                        decodedText += Environment.NewLine;
+                    }
+
+                    else if (modifiedByte == 20)
+                    {
+                        decodedText += '&';
+                    }
+                    else if (modifiedByte == 21)
+                    {
+                        decodedText += ':';
+                    }
+
+
+                    // Sinais de controle (não-imprimíveis)
+                    else if (modifiedByte == 13)
+                    {
+                        decodedText += "\\r";
+                    }
+
+                    // Caracteres legíveis
+                    else if ((modifiedByte >= 32 && modifiedByte <= 126) || modifiedByte == 219 || modifiedByte == 20 || modifiedByte == 21 || modifiedByte == 13)
+                    {
+                        decodedText += (char)modifiedByte;
+                    }
+                    else
+                    {
+                        decodedText += "'" + modifiedByte + "'";
+                    }
+                }
+
+                // Exibir o resultado na TextBox
+                textBox1.Text = decodedText.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error processing file: " + ex.Message);
+            }
+        }
+    }
+}
